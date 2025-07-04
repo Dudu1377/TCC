da4b7825045ea8989fd6c4c1f9d330fc5c2815c3
Dudu1377 <dududetoledo@gmail.com>
2025-07-04 13:53:12 -0300

    Adicionei função de validação para os formularios de Login e Sing Up

--- scripts.js
+++ scripts.js
@@ -11,15 +11,80 @@ let loginButton = document.querySelector('.Login img');
 let imgPassword = document.querySelector('.Input img');
 let togglePassword = document.querySelectorAll('.togglePassword');
 let passwordInput = document.getElementById('password');
-let singIn = document.querySelector('.Sing-in')
+let singUp = document.querySelector('.Sing-up')
 let create = document.querySelector('.create-an-account')
 let popUpCreate = document.querySelector('.pop-up-create')
-let popUpSingIn = document.querySelector('.pop-up-Sing-in')
+let popUpSingUp = document.querySelector('.pop-up-Sing-up') 
 
 let active = 0
 let firstPosition = 0
 let lastPosition = itens.length - 1 // pq começa no 0, ou seja o primeiro item é 0, ai se tem 4 itens, o ultimo item vai ser o 3 [0,1,2,3]
 
+function validateFieldsLogin(){
+
+    const PasswordValid = isPasswordValid();
+    const emailValid = isEmailValid();
+    const confirmPassword = isConfirmPasswordValid();
+    const usernameValid = isUsernameValid();
+    document.getElementById('loginButtonForm').disabled = !emailValid || !PasswordValid || !confirmPassword ||!usernameValid ;
+    
+}
+function validateFieldsSingUp(){
+    const emailSingUpValid = isEmailSingUpValid();
+    document.getElementById('recoverPassword').disabled = !emailSingUpValid;
+
+    const PasswordSingUpValid = isPasswordSingUpValid();
+    document.getElementById('singUpButton').disabled = !emailSingUpValid || !PasswordSingUpValid;
+}
+function isEmailValid(){
+    const email = document.getElementById('email').value;
+    if (!email) {
+        return false;
+    }
+    return validateEmail(email);
+}
+function isPasswordValid(){
+    const password = document.getElementById('password').value;
+    if (!password){
+        return false;
+    }
+    return true;
+}
+function isConfirmPasswordValid (){
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+    if (!confirmPassword || password !== confirmPassword){
+        return false;
+    }
+    return true;
+}
+function isUsernameValid(){
+    const username = document.getElementById('username').value;
+    if (!username){
+        return false;
+    }
+    return true;
+}
+function isEmailSingUpValid(){
+    const emailSingUp = document.getElementById('emailSingUp').value;
+    if (!emailSingUp) {
+        return false;
+    }
+    return validateEmail2(emailSingUp);
+}
+function isPasswordSingUpValid(){
+    const passwordSingUp = document.getElementById('passwordSingUp').value; /* CONECTAR COM BD PARA ACHAR A SENHA DE ACORDO COM O EMAIL */
+    if (!passwordSingUp){
+        return false;
+    }
+    return true;
+}
+function validateEmail(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+function validateEmail2(emailSingUp) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailSingUp);
+}
 
 function setSlider() { //function pq os dois botões basicamente utilizam os mesmos codigos, ai pra evitar a repetição, utiliza ele
     let itemOld = container.querySelector('.list .item.active')//cria uma variavel, para o iten que ele ira tirar da tela, assim ele vai procurar dentro do container um item dentro de list que tem a classe item e active ao mesmo tempo, ou seja o item ativo/que esta mostrando no momento
@@ -32,7 +97,6 @@ function setSlider() { //function pq os dois botões basicamente utilizam os mes
 
     indicator.querySelector('.number').innerText = '0' + (active + 1)//Basicamente procura a classe .number e utiliza o metodo .innerText que altera o texto, ai ele faz o active, que seria a posição dentro dos itens que ele está, + 1, para ir mudando a cada clique, (o 0 é só charme) 
 }
-
 
 nextButton.onclick = () => { //basicamente falando "se esse botao for clicado, faça isso"
     active = active + 1 > lastPosition ? 0 : active + 1 //if else resumido, se active+1 for > que lastposition ele adiciona o valor 0 para ele, caso contrario ele realiza a operação active + 1 sem problemas
@@ -50,16 +114,16 @@ prevButton.onclick = () => {
 loginButton.addEventListener('click', function () {
     popUpContainer.style.display = 'flex';
     popUpCreate.style.display = 'flex'
-    popUpSingIn.style.display = 'none'
+    popUpSingUp.style.display = 'none'
     document.body.classList.add('no-scroll');//adiciona uma classe ao documento
 })
-singIn.addEventListener('click', function () {
+singUp.addEventListener('click', function () {
     popUpCreate.style.display = 'none'
-    popUpSingIn.style.display = 'flex'
+    popUpSingUp.style.display = 'flex'
 })
 create.addEventListener('click', function () {
     popUpCreate.style.display = 'flex'
-    popUpSingIn.style.display = 'none'
+    popUpSingUp.style.display = 'none'
 })
 closeButton.forEach(button => {
 button.addEventListener('click', function () {
